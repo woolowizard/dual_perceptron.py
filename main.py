16ea74fe54f04861ac890b67b0f667e8a5088e2b
woolowizard <42813076+woolowizard@users.noreply.github.com>
2025-07-03 15:15:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ X = X.to_numpy()
 model = DualPerceptron(kernel='rbf', patience=10)
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
-    test_size=0.2,
-    random_state=42,
-    stratify=y
-)
+                                    X, y,
+                                    test_size=0.2,
+                                    random_state=42,
+                                    stratify=y
+                                )
 
 model.fit(X_train, y_train, X_test, y_test)
 
